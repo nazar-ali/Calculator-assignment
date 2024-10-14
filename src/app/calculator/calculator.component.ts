154e418b96702ea5d7837149abafef85ab85cf60
Nazar Ali <nazaralibaloch3@gmail.com>
2024-10-15 00:23:30 +0500

    adding functionality

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component,OnInit } from '@angular/core';
-import {FormGroup,FormControl, ReactiveFormsModule, FormsModule} from "@angular/forms"
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,20 +8,56 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-   
+
   inputStr: any;
 
   ngOnInit(): void {
     this.inputStr = new FormGroup({
-      text : new FormControl()
-    })
+      text: new FormControl('')
+    });
   }
 
-  buttonClick(buttonElement:any){
+  buttonClick(buttonElement: any) {
     let buttonText = buttonElement.textContent;
-    this.inputStr.controls.text.setValue(buttonText)
-  }     
+    let currentText = this.inputStr.controls.text.value.replace(/,/g, ''); 
+
+    if (currentText != null) {
+      currentText += buttonText;
+      this.inputStr.controls.text.setValue(this.byAddingCommasBothIntegerAndDecimal(currentText)); 
+    } else {
+      this.inputStr.controls.text.setValue(buttonText);
+    }
+  }
+
+  clearDisplay() {
+    this.inputStr.controls.text.setValue('');
+  }
+
+  calculatingValues() {
+    const expression = this.inputStr.controls.text.value.replace(/,/g, ''); 
+
+    try {
+      const result = eval(expression); 
+      this.inputStr.controls.text.setValue(this.byAddingCommasBothIntegerAndDecimal(result)); 
+    } catch (e) {
+      this.inputStr.controls.text.setValue('Error');
+    }
+  }
+
+  deletLastItem() {
+    let str = this.inputStr.controls.text.value.replace(/,/g, ''); 
+    str = str.slice(0, -1);
+    this.inputStr.controls.text.setValue(this.byAddingCommasBothIntegerAndDecimal(str)); 
+  }
+
+ 
+  byAddingCommasBothIntegerAndDecimal(value: string): string {
+    const [integerPart, decimalPart] = value.split('.'); 
+    const regixAddCommasAfterThreeDigits = /\B(?=(\d{3})+(?!\d))/g
+    const formattedInteger = integerPart.replace(regixAddCommasAfterThreeDigits,','); 
+    return decimalPart ? `${formattedInteger}.${decimalPart}` : formattedInteger; 
+  }
 }
